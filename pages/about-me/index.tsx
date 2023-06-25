0f2cfbe5a3774eccf108ffb0d64ed11d056bf363
Gabriel Tinoco <gabrielslai@gmail.com>
2023-06-24 20:06:17 -0400

    feat(structure): Created About Me and Projects pages

---
+++ pages/about-me/index.tsx
@@ -0,0 +1,30 @@
+export default function AboutMe() {
+	return (
+		<>
+			<main>
+				<h1>About Me</h1>
+				<p>
+					My name is Gabriel, Im 20 years old, and a software engineering
+					student at Centennial College. Nice to meet you!
+					<br />I started browsing the internet since I was very young, but I
+					properly started my journey in the software/hardware world when I was
+					about 8 years old and got my first laptop. At first, I was mostly
+					interested in playing games (something I still do passionately to this
+					day), but after some time I also discovered the art of graphical
+					design and video editing through YouTube, which became a huge part of
+					what I would do for the next couple of years. I taught myself how to
+					edit images using Photoshop, post-processing on After Effects, editing
+					on Sony Vegas, animation on Flash, and 3D modeling on Cinema 4D.
+					<br />
+					As I got older and more mature (around 13 years old), my parents gave
+					me with my first proper desktop computer. This enabled me to not only{' '}
+					<s>further fuel my gaming addiction</s> expand my graphical design
+					skills, but it also got me started into the more technical side of
+					videogames and computers as well. I learned how to make game servers,
+					how to write simple scripting programs, what were JSON files, and how
+					to make simple web pages using HTML and CSS.
+				</p>
+			</main>
+		</>
+	);
+}
