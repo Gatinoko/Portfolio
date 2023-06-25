31f0722e0caa434583277f2a2ff1eb63a0684ccd
Gabriel Tinoco <gabrielslai@gmail.com>
2023-06-25 10:09:14 -0400

    refactor(components): Updated imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-key */
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Navigation from '@/components/navigation/Navigation';
-import NavButton from '@/components/navigation/nav-button/NavButton';
+import Navigation from '@/components/navigation/navigation';
+import NavButton from '@/components/navigation/nav-button/nav-button';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
