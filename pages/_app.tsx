d31af98e7bf316bd341fc0c91b0079d70419854e
Gabriel Tinoco <gabrielslai@gmail.com>
2023-06-25 15:28:44 -0400

    feat(stylesheets): Added index section styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,9 @@ export default function App({ Component, pageProps }: AppProps) {
 					/>,
 				]}
 			/>
-			<Component {...pageProps} />
+			<main>
+				<Component {...pageProps} />
+			</main>
 		</>
 	);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,12 @@ import styles from './index.module.scss';
 export default function Index() {
 	return (
 		<>
-			<main className={`${styles['index-main']}`}>
+			<div className='index-section'>
 				<div className={`${styles['index-title']}`}>
 					<h1>Hi, I'm Gabriel.</h1>
 					<h4>Developer & designer.</h4>
 				</div>
-			</main>
+			</div>
 		</>
 	);
 }
