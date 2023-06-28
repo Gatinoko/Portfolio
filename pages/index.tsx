0606d3e23a8f5148dff3608d71d0cf1c77b6795e
Gabriel Tinoco <gabrielslai@gmail.com>
2023-06-27 22:24:16 -0400

    fix(pages): Fixed navigation id issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,30 +5,24 @@ import ContentSection from '@/components/navigation/content-section/content-sect
 export default function Index(props: any, rootMainRef: any) {
 	return (
 		<main id='root-main'>
-			<ContentSection>
-				<div
-					id='IntroSection'
-					className={`${styles['index-title']}`}>
+			<ContentSection id='IntroSection'>
+				<div className={`${styles['index-title']}`}>
 					<RevealAnimation>
 						<h1>Hi, I'm Gabriel.</h1>
 						<h4>Developer & designer.</h4>
 					</RevealAnimation>
 				</div>
 			</ContentSection>
-			<ContentSection>
-				<div
-					id='AboutMeSession'
-					className={`${styles['index-title']}`}>
+			<ContentSection id='AboutMeSession'>
+				<div className={`${styles['index-title']}`}>
 					<RevealAnimation>
 						<h1>About me.</h1>
 						<h4>Developer & designer.</h4>
 					</RevealAnimation>
 				</div>
 			</ContentSection>
-			<ContentSection>
-				<div
-					id='ProjectsSection'
-					className={`${styles['index-title']}`}>
+			<ContentSection id='ProjectsSection'>
+				<div className={`${styles['index-title']}`}>
 					<RevealAnimation>
 						<h1>Projects.</h1>
 						<h4>Developer & designer.</h4>
