1d1bce6ff2633cedc9b1f7d06a66c5bcf8afeedb
Gabriel Tinoco <gabrielslai@gmail.com>
2023-08-27 08:30:03 -0400

    feat(components): Added custom sizing support for NavButton component

--- components/navigation/nav-button/nav-button.tsx
+++ components/navigation/nav-button/nav-button.tsx
@@ -1,13 +1,42 @@
-type Props = {
+import { cva } from 'class-variance-authority';
+
+export type NavButtonSizes =
+	| 'extra-large'
+	| 'large'
+	| 'regular'
+	| 'small'
+	| 'extra-small';
+
+type NavButtonProps = {
 	text: string;
+	size?: NavButtonSizes;
 	href: string;
 };
 
-export default function NavButton({ text, href }: Props) {
+const navButton = cva('nav-button', {
+	variants: {
+		size: {
+			auto: 'auto',
+			['extra-large']: 'extra-large',
+			large: 'large',
+			regular: 'regular',
+			medium: 'medium',
+			small: 'small',
+			['extra-small']: 'extra-small',
+		},
+	},
+	defaultVariants: {
+		size: 'auto',
+	},
+});
+
+export default function NavButton(props: NavButtonProps) {
+	const { text, href, size = 'auto' } = props;
+
 	return (
 		<>
 			<a
-				className={'nav-button'}
+				className={navButton({ size })}
 				href={href}>
 				<span className={'nav-button-text'}>{text}</span>
 			</a>
