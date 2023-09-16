092123acbf722741ac6ce3cd986bec18a5b3ed3e
Gabriel Tinoco <gabrielslai@gmail.com>
2023-09-16 18:54:31 -0400

    Finished first draft of navigation

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,26 +1,71 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
+import { cva } from 'class-variance-authority';
 import styles from './navigation.module.scss';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import Button from '../button/button';
 
 type Props = {
 	navButtons?: ReactNode;
 };
 
-export default function Navigation({ navButtons }: Props) {
+const navigation = cva('navigation', {
+	variants: {
+		open: {
+			true: 'open',
+			false: 'closed',
+		},
+	},
+	defaultVariants: {
+		open: true,
+	},
+});
+
+export default function Navigation(props: Props) {
+	const { navButtons } = props;
+
+	const [open, setOpen] = useState(true);
+
 	return (
 		<>
-			<nav className={'navbar'}>
-				{/* Logo */}
-				<a
-					className={'nav-logo'}
-					href='#IntroSection'>
-					GBRL
-					<br />
-					TNco
-				</a>
+			<nav className={navigation({ open })}>
+				{/* Navigation */}
+				<div className='logo-button-container'>
+					{/* Logo */}
+					<a
+						className='nav-logo'
+						href='#IntroSection'>
+						GBRL
+						<br />
+						TNco
+					</a>
 
-				{/* Navigation list */}
-				<div className={'nav-list'}>{navButtons}</div>
+					{/* Desktop navigation buttons */}
+					<ul className='section-list'>
+						<li>
+							<Button
+								className='tech-and-skills-button'
+								size={'extra-small'}
+								text='Tech & Skills'
+							/>
+						</li>
+						<li>
+							<Button
+								className='projects-button'
+								size={'extra-small'}
+								text='Projects'
+							/>
+						</li>
+					</ul>
+
+					{/* Mobile navigation hamburguer button */}
+					<Button
+						className='mobile-hamburguer'
+						prefixIcon={'left-chevron'}
+						size={'extra-small'}
+						onClick={() => setOpen(!open)}
+					/>
+				</div>
+				<div className='nav-list'>{navButtons}</div>
 			</nav>
 		</>
 	);
