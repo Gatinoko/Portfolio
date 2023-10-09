3df5cefe7730c2281be6c91f05d5d9f149ba4537
Gabriel Tinoco <gabrielslai@gmail.com>
2023-10-09 18:48:51 -0400

    feat(components): Added github and link icon to Icon component

--- components/icon/icon.tsx
+++ components/icon/icon.tsx
@@ -16,11 +16,6 @@ export interface IIcon {
 
 const icon = cva('svg', {
 	variants: {
-		name: {
-			['left-chevron']: 'left-chevron',
-			['right-chevron']: 'right-chevron',
-			default: 'default',
-		},
 		size: {
 			['extra-large']: 'extra-large',
 			large: 'large',
@@ -42,7 +37,6 @@ export default function Icon(props: IIcon) {
 				<svg
 					onClick={onClick}
 					className={`${customClassValue} ${icon({
-						name,
 						size,
 					})}`}
 					xmlns='http://www.w3.org/2000/svg'
@@ -59,7 +53,6 @@ export default function Icon(props: IIcon) {
 				<svg
 					onClick={onClick}
 					className={`${customClassValue} ${icon({
-						name,
 						size,
 					})}`}
 					xmlns='http://www.w3.org/2000/svg'
@@ -72,12 +65,37 @@ export default function Icon(props: IIcon) {
 					/>
 				</svg>
 			);
+		case 'github':
+			return (
+				<svg
+					onClick={onClick}
+					className={`${customClassValue} ${icon({
+						size,
+					})}`}
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 16 16'
+					fill='none'>
+					<path d='M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z'></path>
+				</svg>
+			);
+		case 'link':
+			return (
+				<svg
+					onClick={onClick}
+					className={`${customClassValue} ${icon({
+						size,
+					})}`}
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 20 20'
+					fill='none'>
+					<path d='M7.859 14.691l-.81.805a1.814 1.814 0 0 1-2.545 0 1.762 1.762 0 0 1 0-2.504l2.98-2.955c.617-.613 1.779-1.515 2.626-.675a.992.992 0 1 0 1.397-1.407c-1.438-1.428-3.566-1.164-5.419.675l-2.98 2.956A3.719 3.719 0 0 0 2 14.244a3.72 3.72 0 0 0 1.108 2.658 3.779 3.779 0 0 0 2.669 1.096c.967 0 1.934-.365 2.669-1.096l.811-.805a.988.988 0 0 0 .005-1.4.995.995 0 0 0-1.403-.006zm9.032-11.484c-1.547-1.534-3.709-1.617-5.139-.197l-1.009 1.002a.99.99 0 1 0 1.396 1.406l1.01-1.001c.74-.736 1.711-.431 2.346.197.336.335.522.779.522 1.252s-.186.917-.522 1.251l-3.18 3.154c-1.454 1.441-2.136.766-2.427.477a.99.99 0 1 0-1.396 1.406c.668.662 1.43.99 2.228.99.977 0 2.01-.492 2.993-1.467l3.18-3.153A3.732 3.732 0 0 0 18 5.866a3.726 3.726 0 0 0-1.109-2.659z'></path>
+				</svg>
+			);
 		default:
 			return (
 				<svg
 					onClick={onClick}
 					className={`${customClassValue} ${icon({
-						name: 'default',
 						size,
 					})}`}
 					xmlns='http://www.w3.org/2000/svg'
