43943292349e42d80b8e4b9f71ded1dee752f4ab
Gabriel Tinoco <gabrielslai@gmail.com>
2023-10-14 18:26:15 -0400

    feat(components): Created ColumnGrid component

--- components/column-grid/column-grid.tsx
+++ components/column-grid/column-grid.tsx
@@ -21,19 +21,28 @@ export default function ColumnGrid(props: ColumnGridProps) {
 	const { platform = 'desktop', className = '' } = props;
 
 	return (
-		<div className={`${columnGrid({ platform })}`}>
-			<div className='col1 col' />
-			<div className='col2 col' />
-			<div className='col3 col' />
-			<div className='col4 col' />
-			<div className='col5 col' />
-			<div className='col6 col' />
-			<div className='col7 col' />
-			<div className='col8 col' />
-			<div className='col9 col' />
-			<div className='col10 col' />
-			<div className='col11 col' />
-			<div className='col12 col' />
+		<div
+			style={{
+				display: 'flex',
+				width: '100%',
+				height: '100%',
+				justifyContent: 'center',
+				position: 'fixed',
+			}}>
+			<div className={`${columnGrid({ platform })}`}>
+				<div className='col1 col' />
+				<div className='col2 col' />
+				<div className='col3 col' />
+				<div className='col4 col' />
+				<div className='col5 col' />
+				<div className='col6 col' />
+				<div className='col7 col' />
+				<div className='col8 col' />
+				<div className='col9 col' />
+				<div className='col10 col' />
+				<div className='col11 col' />
+				<div className='col12 col' />
+			</div>
 		</div>
 	);
 }
