204ca2105e71236097beef6ec1f0494e659f4e48
Gabriel Tinoco <gabrielslai@gmail.com>
2023-12-20 19:35:52 -0500

    feat(pages): Implemented rough first draft index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,352 +14,344 @@ import ColumnGrid from '@/components/column-grid/column-grid';
 export default function Index(props: any, rootMainRef: any) {
 	return (
 		<>
-			{/* <ColumnGrid platform={'responsive'} /> */}
+			<ColumnGrid platform={'responsive'} />
 			<main
 				className={'main'}
 				id='root-main'>
 				{/* Intro section */}
-				<RevealAnimation>
-					<PageSection
-						id='IntroSection'
-						className={'index-title'}>
-						<h1 style={{ width: '6ch' }}>Gabriel Tinoco.</h1>
-						<h4>Software Engineer & Designer</h4>
-						<hr />
-						<p>
-							Career statement goes here. Should be something like really
-							impactful. Lorem ipsum dolor sit amet, consectetur adipiscing
-							elit. Morbi non sapien ante. Suspendisse vel consectetur urna. Ut
-							a dictum sem. Praesent faucibus nisi ac fermentum posuere.
-						</p>
-					</PageSection>
-				</RevealAnimation>
+				<PageSection
+					id='IntroSection'
+					className={'index-title'}>
+					<h1 style={{ width: '6ch' }}>Gabriel Tinoco.</h1>
+					<h4>Software Engineer & Designer</h4>
+					<hr />
+					<p>
+						Career statement goes here. Should be something like really
+						impactful. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Morbi non sapien ante. Suspendisse vel consectetur urna. Ut a dictum
+						sem. Praesent faucibus nisi ac fermentum posuere.
+					</p>
+				</PageSection>
 
 				{/* Tech & skills section */}
-				<RevealAnimation>
-					<PageSection
-						id='Tech&SkillsSection'
-						className={'information-page'}>
-						<h2>Tech & Skills.</h2>
-						<hr />
-						<ChipCloud
-							itemArray={[
-								{ text: 'TypeScript', weight: 5 },
-								{ text: 'Node', weight: 3 },
-								{ text: 'Angular', weight: 5 },
-								{ text: 'MySQL', weight: 1 },
-								{ text: 'GIT', weight: 4 },
-								{ text: 'CSS', weight: 5 },
-								{ text: 'Storybook', weight: 4 },
-								{ text: 'Java', weight: 3 },
-								{ text: 'Python', weight: 2 },
-								{ text: 'Spring', weight: 3 },
-								{ text: 'MongoDB', weight: 3 },
-								{ text: 'JavaScript', weight: 5 },
-								{ text: 'Oracle SQL', weight: 1 },
-								{ text: 'JWT', weight: 3 },
-								{ text: 'Prettier', weight: 3 },
-								{ text: 'ESLint', weight: 3 },
-								{ text: 'Nodemon', weight: 3 },
-								{ text: 'Sass', weight: 5 },
-								{ text: 'GitHub', weight: 4 },
-								{ text: 'Next.js', weight: 5 },
-								{ text: 'CVA', weight: 3 },
-								{ text: 'React', weight: 5 },
-								{ text: 'GraphQL', weight: 2 },
-								{ text: 'HTML', weight: 5 },
-								{ text: 'Lodash', weight: 1 },
-								{ text: 'Jira', weight: 4 },
-								{ text: 'Jenkins', weight: 2 },
-								{ text: 'Express.js', weight: 3 },
-								{ text: 'CSHARP', weight: 4 },
-								{ text: 'XML', weight: 2 },
-								{ text: 'JSON', weight: 4 },
-								{ text: 'Figma', weight: 5 },
-								{ text: 'Adobe Photoshop', weight: 5 },
-								{ text: 'Adobe Premiere', weight: 4 },
-							]}
-							size='regular'
-						/>
-					</PageSection>
-				</RevealAnimation>
+				<PageSection
+					id='Tech&SkillsSection'
+					className={'information-page'}>
+					<h3 className='section-title'>Tech & Skills.</h3>
+					<hr />
+					<ChipCloud
+						itemArray={[
+							{ text: 'TypeScript', weight: 5 },
+							{ text: 'Node', weight: 3 },
+							{ text: 'Angular', weight: 5 },
+							{ text: 'MySQL', weight: 1 },
+							{ text: 'GIT', weight: 4 },
+							{ text: 'CSS', weight: 5 },
+							{ text: 'Storybook', weight: 4 },
+							{ text: 'Java', weight: 3 },
+							{ text: 'Python', weight: 2 },
+							{ text: 'Spring', weight: 3 },
+							{ text: 'MongoDB', weight: 3 },
+							{ text: 'JavaScript', weight: 5 },
+							{ text: 'Oracle SQL', weight: 1 },
+							{ text: 'JWT', weight: 3 },
+							{ text: 'Prettier', weight: 3 },
+							{ text: 'ESLint', weight: 3 },
+							{ text: 'Nodemon', weight: 3 },
+							{ text: 'Sass', weight: 5 },
+							{ text: 'GitHub', weight: 4 },
+							{ text: 'Next.js', weight: 5 },
+							{ text: 'CVA', weight: 3 },
+							{ text: 'React', weight: 5 },
+							{ text: 'GraphQL', weight: 2 },
+							{ text: 'HTML', weight: 5 },
+							{ text: 'Lodash', weight: 1 },
+							{ text: 'Jira', weight: 4 },
+							{ text: 'Jenkins', weight: 2 },
+							{ text: 'Express.js', weight: 3 },
+							{ text: 'CSHARP', weight: 4 },
+							{ text: 'XML', weight: 2 },
+							{ text: 'JSON', weight: 4 },
+							{ text: 'Figma', weight: 5 },
+							{ text: 'Adobe Photoshop', weight: 5 },
+							{ text: 'Adobe Premiere', weight: 4 },
+						]}
+						size='regular'
+					/>
+				</PageSection>
 
 				{/* Timeline section */}
-				<RevealAnimation>
-					<PageSection
-						id='TimelineSection'
-						className={'information-page'}>
-						<h2>Timeline</h2>
-						<hr />
-						<WorkTimeline
-							workTimelineItems={[
-								<TimelineItem
-									key='whimtach'
-									title={'WHIMTACH'}
-									date={'(6 months)'}
-									subitemArray={[
-										<TimelineSubitem
-											key={'ux designer'}
-											startDate={'Feb 2022'}
-											endDate={'May 2022'}
-											subtitle={'UX Designer'}
-											description={`Acted as the lead UX designer in the elaboration of a cosmetic business’s website, creating multiple
+				<PageSection
+					id='TimelineSection'
+					className={'information-page'}>
+					<h3 className='section-title'>Timeline</h3>
+					<hr />
+					<WorkTimeline
+						workTimelineItems={[
+							<TimelineItem
+								key='whimtach'
+								title={'WHIMTACH'}
+								date={'(6 months)'}
+								subitemArray={[
+									<TimelineSubitem
+										key={'ux designer'}
+										startDate={'Feb 2022'}
+										endDate={'May 2022'}
+										subtitle={'UX Designer'}
+										description={`Acted as the lead UX designer in the elaboration of a cosmetic business’s website, creating multiple
 											page designs in Figma and conceptual designs in Photoshop, as well as having biweekly meetings with
 											the project’s stakeholders in order to gather feedback and discuss requirements`}
-											icons={[
-												<Icon
-													key={'photoshop'}
-													name={'photoshop'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'figma'}
-													name={'figma'}
-													size={'small'}
-												/>,
-											]}
-										/>,
-									]}
-								/>,
-								<TimelineItem
-									key='RBC (Royal Bank of Canada)'
-									date={'(1 year)'}
-									title={'RBC (Royal Bank of Canada)'}
-									subitemArray={[
-										<TimelineSubitem
-											key={'Full-stack Developer Co-op'}
-											startDate={'May 2022'}
-											endDate={'Ago 2022'}
-											subtitle={'Full-stack Developer Co-op'}
-											description={`Worked on developing a MEAN stack application for a small team of people in order to speed up data gathering and visualization`}
-											icons={[
-												<Icon
-													key={'nodejs'}
-													name={'nodejs'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'angular'}
-													name={'angular'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'html5'}
-													name={'html5'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'css3'}
-													name={'css3'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'mongodb'}
-													name={'mongodb'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'express'}
-													name={'express'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'typescript'}
-													name={'typescript'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'python'}
-													name={'python'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'bootstrap'}
-													name={'bootstrap'}
-													size={'small'}
-												/>,
-											]}
-										/>,
-										<TimelineSubitem
-											key={'Front-end Developer Co-op'}
-											startDate={'Sep 2022'}
-											endDate={'Ago 2023'}
-											subtitle={'Front-end Developer Co-op'}
-											description={`Implemented, tested, and reviewed Angular/React components at the company's main front-end component library team`}
-											icons={[
-												<Icon
-													key={'nodejs'}
-													name={'nodejs'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'angular'}
-													name={'angular'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'react'}
-													name={'react'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'html5'}
-													name={'html5'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'css3'}
-													name={'css3'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'sass'}
-													name={'sass'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'storybook'}
-													name={'storybook'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'nx'}
-													name={'nx'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'jenkins'}
-													name={'jenkins'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'jest'}
-													name={'jest'}
-													size={'small'}
-												/>,
-												<Icon
-													key={'figma'}
-													name={'figma'}
-													size={'small'}
-												/>,
-											]}
-										/>,
-									]}
-								/>,
-							]}
-						/>
-					</PageSection>
-				</RevealAnimation>
+										icons={[
+											<Icon
+												key={'photoshop'}
+												name={'photoshop'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'figma'}
+												name={'figma'}
+												size={'small'}
+											/>,
+										]}
+									/>,
+								]}
+							/>,
+							<TimelineItem
+								key='RBC (Royal Bank of Canada)'
+								date={'(1 year)'}
+								title={'RBC (Royal Bank of Canada)'}
+								subitemArray={[
+									<TimelineSubitem
+										key={'Full-stack Developer Co-op'}
+										startDate={'May 2022'}
+										endDate={'Ago 2022'}
+										subtitle={'Full-stack Developer Co-op'}
+										description={`Worked on developing a MEAN stack application for a small team of people in order to speed up data gathering and visualization`}
+										icons={[
+											<Icon
+												key={'nodejs'}
+												name={'nodejs'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'angular'}
+												name={'angular'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'html5'}
+												name={'html5'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'css3'}
+												name={'css3'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'mongodb'}
+												name={'mongodb'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'express'}
+												name={'express'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'typescript'}
+												name={'typescript'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'python'}
+												name={'python'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'bootstrap'}
+												name={'bootstrap'}
+												size={'small'}
+											/>,
+										]}
+									/>,
+									<TimelineSubitem
+										key={'Front-end Developer Co-op'}
+										startDate={'Sep 2022'}
+										endDate={'Ago 2023'}
+										subtitle={'Front-end Developer Co-op'}
+										description={`Implemented, tested, and reviewed Angular/React components at the company's main front-end component library team`}
+										icons={[
+											<Icon
+												key={'nodejs'}
+												name={'nodejs'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'angular'}
+												name={'angular'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'react'}
+												name={'react'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'html5'}
+												name={'html5'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'css3'}
+												name={'css3'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'sass'}
+												name={'sass'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'storybook'}
+												name={'storybook'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'nx'}
+												name={'nx'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'jenkins'}
+												name={'jenkins'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'jest'}
+												name={'jest'}
+												size={'small'}
+											/>,
+											<Icon
+												key={'figma'}
+												name={'figma'}
+												size={'small'}
+											/>,
+										]}
+									/>,
+								]}
+							/>,
+						]}
+					/>
+				</PageSection>
 
 				{/* Projects section */}
-				<RevealAnimation>
-					<PageSection
-						id='ProjectsSection'
-						className={'information-page'}>
-						<h2>Projects</h2>
-						<hr />
-						<ProjectBox
-							projectBoxItems={[
-								<ProjectItem
-									key={'Bracketeams Tournament Website'}
-									githubProjectLink='https://github.com/Gatinoko/bracketeamsProject'
-									title='Bracketeams Tournament Website'
-									description={`Collaborated in a group of 6 people as a lead software engineer in order to create a functional brackets
+				<PageSection
+					id='ProjectsSection'
+					className={'information-page'}>
+					<h3 className='section-title'>Projects</h3>
+					<hr />
+					<ProjectBox
+						projectBoxItems={[
+							<ProjectItem
+								key={'Bracketeams Tournament Website'}
+								githubProjectLink='https://github.com/Gatinoko/bracketeamsProject'
+								title='Bracketeams Tournament Website'
+								description={`Collaborated in a group of 6 people as a lead software engineer in order to create a functional brackets
 							website, where the user could create, read, edit, and delete game tournaments`}
-									icons={[
-										<Icon
-											key={'1'}
-											name={'nodejs'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'angular'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'html5'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'css3'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'mongodb'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'express'}
-											size={'small'}
-										/>,
-									]}
-								/>,
-								<ProjectItem
-									key={'Personal Website'}
-									title='Personal Website'
-									githubProjectLink='https://github.com/Gatinoko/GabrielTinocoWebsite'
-									description={`Created a website with information about my projects, services, contact information, and a login page`}
-									icons={[
-										<Icon
-											key={'1'}
-											name={'nodejs'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'html5'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'css3'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'mongodb'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'express'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'ejs'}
-											size={'small'}
-										/>,
-									]}
-								/>,
-								<ProjectItem
-									key={'Mockup Music Player'}
-									title='Mockup Music Player'
-									githubProjectLink='https://github.com/Gatinoko/COMP123-Assignment4'
-									description={`Implemented the following features: add and delete artists, assign music to specific artists, edit music
+								icons={[
+									<Icon
+										key={'1'}
+										name={'nodejs'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'angular'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'html5'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'css3'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'mongodb'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'express'}
+										size={'small'}
+									/>,
+								]}
+							/>,
+							<ProjectItem
+								key={'Personal Website'}
+								title='Personal Website'
+								githubProjectLink='https://github.com/Gatinoko/GabrielTinocoWebsite'
+								description={`Created a website with information about my projects, services, contact information, and a login page`}
+								icons={[
+									<Icon
+										key={'1'}
+										name={'nodejs'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'html5'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'css3'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'mongodb'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'express'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'ejs'}
+										size={'small'}
+									/>,
+								]}
+							/>,
+							<ProjectItem
+								key={'Mockup Music Player'}
+								title='Mockup Music Player'
+								githubProjectLink='https://github.com/Gatinoko/COMP123-Assignment4'
+								description={`Implemented the following features: add and delete artists, assign music to specific artists, edit music
 								information, delete music, and play the user’s selected music`}
-									icons={[
-										<Icon
-											key={'1'}
-											name={'nodejs'}
-											size={'small'}
-										/>,
-										<Icon
-											key={'1'}
-											name={'angular'}
-											size={'small'}
-										/>,
-									]}
-								/>,
-							]}
-						/>
-					</PageSection>
-				</RevealAnimation>
+								icons={[
+									<Icon
+										key={'1'}
+										name={'nodejs'}
+										size={'small'}
+									/>,
+									<Icon
+										key={'1'}
+										name={'angular'}
+										size={'small'}
+									/>,
+								]}
+							/>,
+						]}
+					/>
+				</PageSection>
 			</main>
 		</>
 	);
