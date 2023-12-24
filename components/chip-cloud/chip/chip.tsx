a6cc65780d2e714785993108ff2516d800a995c5
Gabriel Tinoco <gabrielslai@gmail.com>
2023-12-24 12:53:32 -0500

    Revised and re-documented Chip component

--- components/chip-cloud/chip/chip.tsx
+++ components/chip-cloud/chip/chip.tsx
@@ -1,6 +1,9 @@
 import { cva } from 'class-variance-authority';
 import React from 'react';
 
+/**
+ * Custom type declaration for the different sizes a `Chip` component can assume.
+ */
 export type ChipSizes =
 	| 'extra-large'
 	| 'large'
@@ -8,15 +11,19 @@ export type ChipSizes =
 	| 'small'
 	| 'extra-small';
 
-interface ChipProps {
+/**
+ * Custom type declaration for the `Chip` React component properties.
+ */
+export type ChipProps = {
 	text: string;
 	size: ChipSizes;
+	type?: 'static' | 'animated';
 	id?: any;
 	link?: string;
 	className?: string;
-}
+};
 
-const tag = cva('chip', {
+const chip = cva('chip', {
 	variants: {
 		size: {
 			['extra-large']: 'extra-large',
@@ -26,28 +33,38 @@ const tag = cva('chip', {
 			small: 'small',
 			['extra-small']: 'extra-small',
 		},
+		type: {
+			animated: 'animated',
+			static: 'static',
+		},
 	},
 	defaultVariants: {
 		size: 'regular',
+		type: 'static',
 	},
 });
 
+/**
+ * React component to-be used inside the `ChipCloud` component or by itself.
+ *
+ * @param {ChipProps} props - Component properties.
+ */
 export default function Chip(props: ChipProps) {
-	const { id, text, size, link, className = '' } = props;
+	const { id, text, size, link, type, className = '' } = props;
 
 	return (
 		<>
 			{'link' in props ? (
 				<a
 					id={id}
-					className={`${tag({ size })} ${className}`}
+					className={`${chip({ size, type })} ${className}`}
 					href={link}>
 					{text}
 				</a>
 			) : (
 				<p
 					id={id}
-					className={`${tag({ size })} ${className}`}>
+					className={`${chip({ size, type })} ${className}`}>
 					{text}
 				</p>
 			)}
