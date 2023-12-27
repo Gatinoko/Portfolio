2e0360c3f0d9e612990006ebff3106391112c070
Gabriel Tinoco <gabrielslai@gmail.com>
2023-12-27 10:45:14 -0500

    Updated dependencies with changes

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Button from '../button/button';
-import Logo from './logo/logo';
+import { Button } from '../button/button';
+import { Logo } from './logo/logo';
 import MobileButton from './mobile-navigation-list/mobile-button/mobile-button';
 import MobileNavigationList from './mobile-navigation-list/mobile-navigation-list';
 
@@ -21,11 +21,7 @@ export default function Navigation(props: NavigationProps) {
 		<nav className='navigation'>
 			<div className='navigation-root'>
 				{/* Logo */}
-				<a
-					className='logo'
-					href='#IntroSection'>
-					<Logo />
-				</a>
+				<Logo href='#IntroSection' />
 
 				{/* Desktop navigation buttons */}
 				<ul className='desktop-button-list'>
