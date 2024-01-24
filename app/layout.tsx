4a7e2d48efc82ef66e7f42a97fb7e25814ce33ba
Gabriel Tinoco <gabrielslai@gmail.com>
2024-01-24 10:10:50 -0500

    perf(stylesheets): Optimized font loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,26 @@ import '../styles/globals.scss';
 import { Footer } from '@/components/footer/footer';
 import { Navigation } from '@/components/navigation/Navigation';
 import { PropsWithChildren } from 'react';
+import { Syne, Major_Mono_Display, Open_Sans } from 'next/font/google';
+
+const syne = Syne({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-syne',
+	weight: ['400', '500', '600', '700'],
+});
+const majorMonoDisplay = Major_Mono_Display({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-major-mono',
+	weight: '400',
+});
+const openSans = Open_Sans({
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-open-sans',
+	weight: ['300', '400', '600'],
+});
 
 export const metadata: Metadata = {
 	title: 'Home',
@@ -12,7 +32,8 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang='en'>
-			<body>
+			<body
+				className={`${syne.variable} ${majorMonoDisplay.variable} ${openSans.variable}`}>
 				<Navigation />
 				{children}
 				<Footer />
